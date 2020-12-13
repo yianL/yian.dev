72021cdf4e0f11139fedd54306167e046c594fb3
andrew lai <andrew.lai@mulesoft.com>
2020-12-12 21:50:47 -0800

    heroku

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
