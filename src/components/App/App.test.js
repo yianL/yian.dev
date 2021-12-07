24861273611e3bc2ac6de691c8772293268c1182
Andrew Lai <andrew.lai@mulesoft.com>
2021-12-07 12:13:17 -0800

    fix test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './';
 
+import data from '../../../public/data.json';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App data={data} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
