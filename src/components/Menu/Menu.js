6d17c6440dee2380696eaedf317c61dde7035e73
Andrew Lai <andrew.lai@mulesoft.com>
2022-03-06 11:05:02 -0800

    add resume link

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -37,6 +37,15 @@ class Menu extends Component {
             src/
           </a>
         </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://docs.google.com/document/d/1VpCJXs3vGY7mk4KDMjQkLSZmTz399kFW-lUGZVxjZXo/edit?usp=sharing"
+          >
+            resume.doc
+          </a>
+        </li>
         <li onClick={onShuffle}>shuffle.sh</li>
       </ul>
     );
