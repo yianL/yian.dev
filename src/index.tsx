0f78cd1c518e26d50d07ca57e315e631bea64173
Yi-An Lai <yian@gem.com>
2023-08-05 09:38:08 -0700

    add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,37 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import App from "./components/App";
 import { Signature } from "./utils/constants";
 import pac from "../package.json";
 
-fetch(`${process.env.PUBLIC_URL}/data.json`)
-  .then((response) => {
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+async function fetchData() {
+  try {
+    const response = await fetch(`${process.env.PUBLIC_URL}/data.json`);
     if (response.status >= 400) {
       throw new Error("Bad response from server");
     }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(Signature, "color: blue; font-weight: bold");
-    console.log("Quick start: yian.skills.map(s => s.name).join(', ')");
-    window.yian = { ...data };
-    const root = ReactDOM.createRoot(
-      document.getElementById("root") as HTMLElement
-    );
-    root.render(<App data={data} version={pac.version} />);
-  });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+(async function init() {
+  const FirebaseConfig = await (await fetch("/__/firebase/init.json")).json();
+  const app = initializeApp(FirebaseConfig);
+  // Initialize Analytics
+  getAnalytics(app);
+
+  const data = await fetchData();
+
+  console.log(Signature, "color: blue; font-weight: bold");
+  console.log("Quick start: yian.skills.map(s => s.name).join(', ')");
+  window.yian = { ...data };
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(<App data={data} version={pac.version} />);
+})();
