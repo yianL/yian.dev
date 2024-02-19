84759b3e2025d5bc0fa1a494f22e962c5bcc18c9
Yi-An Lai <yian@gem.com>
2024-02-18 23:48:53 -0800

    remark-html -> remark-rehype

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,9 +1,17 @@
-import { remark } from "remark";
-import html from "remark-html";
+import rehypeFormat from "rehype-format";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
 
 export default async function markdownToHtml(
   markdown: string
 ): Promise<string> {
-  const result = await remark().use(html).process(markdown);
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .process(markdown);
   return result.toString();
 }
