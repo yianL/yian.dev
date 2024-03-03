8ebffa2512224415938850c6d0b63e7c70ff1a31
Yi-An Lai <yian@gem.com>
2024-03-02 16:42:47 -0800

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Noto_Sans } from "next/font/google";
 import Script from "next/script";
 
-import { SiteTitle, SiteDescription } from "../lib/constants";
+import { SiteTitle, SiteDescription, IS_LOCAL_DEV } from "../lib/constants";
 
 import "../styles/index.css";
 
@@ -24,20 +24,51 @@ export default function RootLayout({
   return (
     <html lang="en" className={noto.className}>
       <head>
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-P9MJR96YXY"
-          strategy="beforeInteractive"
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png?v=202403"
         />
-        <Script id="ga" strategy="beforeInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png?v=202403"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png?v=202403"
+        />
+        <link rel="manifest" href="/site.webmanifest?v=202403" />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg?v=202403"
+          color="#4c574a"
+        />
+        <link rel="shortcut icon" href="/favicon.ico?v=202403" />
+        <meta name="msapplication-TileColor" content="#4c574a" />
+        <meta name="theme-color" content="#c6ccc3" />
 
-            gtag('config', 'G-P9MJR96YXY');
-          `}
-        </Script>
+        {!IS_LOCAL_DEV && (
+          <>
+            <Script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-P9MJR96YXY"
+              strategy="beforeInteractive"
+            />
+            <Script id="ga" strategy="beforeInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-P9MJR96YXY');
+              `}
+            </Script>
+          </>
+        )}
         <Script
           src="https://kit.fontawesome.com/87445c11d6.js"
           strategy="beforeInteractive"
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,8 @@
+export const IS_LOCAL_DEV = process.env.NODE_ENV === "development";
+
 export const Suffix = " | Pedal Powered Dev";
 export const SiteTitle = `Yi-An Lai${Suffix}`;
 export const SiteDescription = "Cycle, caffeinate, code, repeat.";
-
 export const Signature = `%c
 __   ___         _            _          _
 \\ \\ / (_)       / \\   _ __   | |    __ _(_)
